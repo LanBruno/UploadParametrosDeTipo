7da6c93dbc760d85fc6d54710de8facb9885797f
LanBruno <lan.brn@hotmail.com>
2023-08-23 21:04:47 -0300

    Primeiro commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿namespace ParametrosDeTipo
+{
+    
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           var p1 = new Pair<int, string>(1, "Um");
+           int i = p1.First;
+           string s = p1.Second;
+
+           var p2 = new Pair<string, double>("Teclado", 45.75);
+           string produto = p2.First; 
+           double valor = p2.Second;
+
+           Console.WriteLine($"Produto: {produto} - Valor: { valor}");
+        }
+    }
+
+    public class Pair<Tfirst, Tsecond>
+    {
+        public Tfirst First { get; }
+        public Tsecond Second { get; }
+
+
+        public Pair(Tfirst first, Tsecond second) => (First, Second) = (first, second); /// =>
+
+
+
+    }             
+}
